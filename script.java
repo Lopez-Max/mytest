aa2ea1b44b64ced6405f7cea69586fe7ab68d193
Lopez-Max <maxlopezAT@gmail.com>
2025-02-07 01:00:54 -0600

    Add files via upload

---
+++ script.java
@@ -0,0 +1,5 @@
+window.addEventListener('load', () => {
+  setTimeout(() => {
+    document.body.classList.remove('not-loaded');
+  }, 500); // delay (in milliseconds) before starting the animation
+});

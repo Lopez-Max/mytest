2928ec098f8fbf6af12efefbeb56079a2400eb30
Lopez-Max <maxlopezAT@gmail.com>
2025-02-07 01:10:57 -0600

    Delete script.java

--- script.java
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', () => {
-  setTimeout(() => {
-    document.body.classList.remove('not-loaded');
-  }, 500); // delay (in milliseconds) before starting the animation
-});
